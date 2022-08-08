0689eb13775ea19ba5b97c8f999fbcd715b5d888
Alexandra Symeonidou <alex_pink1@yahoo.gr>
2022-08-08 11:56:57 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,13 +2,6 @@ import SoundBrowser
 import SoundDB
 
 db = True  # Set True to use database or False to use class instances
-sql_create_sounds_table = """ CREATE TABLE IF NOT EXISTS sounds (
-                                        id integer PRIMARY KEY,
-                                        name text NOT NULL,
-                                        duration NOT NULL,
-                                        path text NOT NULL
-                                    ); """
-sql_check = """SELECT * FROM sounds"""
 
 if __name__ == '__main__':
         if db:
