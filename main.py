4f685ffb2ba72ef454eebcb79890c7e1e72b76a3
Alexandra Symeonidou <alex_pink1@yahoo.gr>
2022-08-08 11:55:57 +0300

    Update main
    add the option of using a database, instead of a class.

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
 import SoundBrowser
+import SoundDB
 
-# Press the green button in the gutter to run the script.
+db = True  # Set True to use database or False to use class instances
+sql_create_sounds_table = """ CREATE TABLE IF NOT EXISTS sounds (
+                                        id integer PRIMARY KEY,
+                                        name text NOT NULL,
+                                        duration NOT NULL,
+                                        path text NOT NULL
+                                    ); """
+sql_check = """SELECT * FROM sounds"""
+
 if __name__ == '__main__':
-    SoundBrowser.play_or_exit()
+        if db:
+        SoundDB.play_or_exit_db()
+    else:
+        SoundBrowser.play_or_exit()
