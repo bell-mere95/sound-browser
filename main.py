e99760ab8d93be886c41fc443cc4fb00e4d9f33e
Alexandra Symeonidou <alex_pink1@yahoo.gr>
2022-08-10 16:32:31 +0300

    Update main

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import SoundDB
 db = True  # Set True to use database or False to use class instances
 
 if __name__ == '__main__':
-        if db:
-        SoundDB.play_or_exit_db()
+    if db:
+        SoundDB.intro()
     else:
         SoundBrowser.play_or_exit()
