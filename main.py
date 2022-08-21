0a0c2ad4ff38ec5c95331053b9645b831ef7388e
Alexandra Symeonidou <alex_pink1@yahoo.gr>
2022-08-21 16:34:31 +0300

    Update main
    add option to use a GUI

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 import SoundBrowser
 import SoundDB
 
-db = True  # Set True to use database or False to use class instances
+db = 1  # Set 1 to use database, 2 to use class instances or 3 to use GUI
 
 if __name__ == '__main__':
-    if db:
+    if db == 1:
         SoundDB.intro()
-    else:
+    elif db == 2:
         SoundBrowser.play_or_exit()
+    elif db == 3:
+        SoundG.main()
+    else:
+        print("You need set db variable as 1, 2 or 3. See the comment above")
